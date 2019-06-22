b3a894008ac31153ec7fbeb75109b805816e97d2
Rakshith1992 <32623125+Rakshith1992@users.noreply.github.com>
2019-06-22 19:21:32 -0300

    Add files via upload

---
+++ table.component.ts
@@ -0,0 +1,23 @@
+/* import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  myJSONArray = [{"id":1,"status":"active","name":"A"},
+{"id":2,"status":"live","name":"B"},
+{"id":3,"status":"active","name":"C"},
+{"id":6,"status":"deleted","name":"D"},
+{"id":4,"status":"live","name":"E"},
+{"id":5,"status":"active","name":"F"}]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+ */
